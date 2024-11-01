d462d778553ce07c0de4db2b2a88e14e1dea531a
ayumi <dottoz2531@gmail.com>
2024-11-01 21:24:02 +0900

    空欄削除

--- functions.php
+++ functions.php
@@ -14,7 +14,6 @@ function my_script_init() {
   wp_enqueue_style("reset-css", get_template_directory_uri() . "/css/reset.css");
   wp_enqueue_style("my", get_template_directory_uri() . "/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all");
   wp_enqueue_script("my", get_template_directory_uri() . "/js/script.js", array("jquery"), filemtime(get_theme_file_path('js/script.js')), true);
-  
 }
   add_action("wp_enqueue_scripts", "my_script_init");
 
